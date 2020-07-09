dea94b7d19c67acfb7ed2ec1e1ac597bb8ab940a
kausr2595 <kausr2595@gmail.com>
2020-07-10 02:04:25 +0530

    Mutation Added

--- backend/graphqlSchema/schema.js
+++ backend/graphqlSchema/schema.js
@@ -2,8 +2,9 @@ const graphql = require('graphql');
 const _ = require('lodash');
 
 //Calling of the models to interact with mongodb
-const content = require('../models/content.js');
-const author = require('../models/author.js');
+const Content = require('../models/content.js');
+const Author = require('../models/author.js');
+
 const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList} = graphql;
 
 const ContentType = new GraphQLObjectType({
@@ -11,10 +12,11 @@ const ContentType = new GraphQLObjectType({
 	fields:()=>({
 		id:{ type: GraphQLID},
 		heading:{type: GraphQLString},
+		article:{type: GraphQLString},
 		author: {
 			type: AuthorType,
 			resolve(parent, args) {
-				// return _.find(authors, {id: parent.authorId});
+				return Author.findById(parent.authorId);
 			}
 		}
 	})
@@ -31,6 +33,7 @@ const AuthorType = new GraphQLObjectType({
 			type : new GraphQLList(ContentType), //As each author contain list of contents
 			resolve(parent, args) {
 				// return _.filter(content, {authorId: parent.id});
+				return Content.find({authorId : parent.id});
 			}
 		}
 	})
@@ -46,7 +49,7 @@ const RootQuery = new GraphQLObjectType({
 			args: {id: { type:GraphQLID }},
 			resolve(parent, args) {
 				//Code to get data from Database
-				// return _.find(content,{id: args.id});
+				return Content.findById(args.id);
 			}
 		},
 		//Query to Return authors of the content on the basis of id
@@ -54,27 +57,67 @@ const RootQuery = new GraphQLObjectType({
 			type:AuthorType,
 			args: {id: {type: GraphQLID}},
 			resolve(parent, args) {
-				// return _.find(authors,{id: args.id});
+				return Author.findById(args.id);
 			}
 		},
 		//Query to Return all the content
 		'contents' : {
 			type: new GraphQLList(ContentType),
 			resolve(parent, args) {
-				return content
+				return Content.find({});
 			}
 		},
 		//Query to Return all the author
 		'authors' : {
 			type: new GraphQLList(AuthorType),
 			resolve(parent, args) {
-				return authors
+				return Author.find({});
 			}
 		},		
 	}
 });
 
+//Setting up Mutation for Performing CRUD operation in Mongo
+const Mutation = new GraphQLObjectType({
+	name : 'Mutation',
+	fields: {
+		addAuthor : {
+			type: AuthorType,
+			args: {                            //Fields we require from Frontend
+				name : {type : GraphQLString},
+				age : {type : GraphQLInt}
+			},
+			resolve(parent, args) {
+				let author = new Author({
+					name : args.name,
+					age : args.age
+				});
+				return author.save();
+			}
+		},
+
+		addContent : {
+			type: ContentType,
+			args : {
+				heading : {type : GraphQLString},
+				article : {type : GraphQLString},
+				authorId : {type : GraphQLID}
+			},
+			resolve(parent, args) {
+				let content = new Content({
+					heading : args.heading,
+					article : args.article,
+					authorId : args.authorId
+				});
+
+				return content.save();
+			}
+		}
+	}
+})
+
 
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation: Mutation
 });
