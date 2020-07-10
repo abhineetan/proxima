2273593749d34db061321e9d9cfdc46838a07d25
kausr2595 <kausr2595@gmail.com>
2020-07-11 01:40:23 +0530

    Article Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Components
+import ArticleList from './components/ArticleList';
+
+class App extends Component{
+	render() {
+		return (
+			<div id="main">
+				<h1> Article List </h1>
+				<ArticleList/>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default ArticleList;
---
+++ client/src/components/ArticleList.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class ArticleList extends Component{
+	render() {
+		return (
+			<div>
+				<ul id="article-list">
+					<li>Article Name</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default ArticleList;
